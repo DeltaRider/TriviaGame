d1483ec255689f05a0865086de2fe945dafd3aca
DeltaRider <deltariderllc@gmail.com>
2019-02-05 20:10:00 -0800

    first question and timeout popup finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
-var time = 30;
+var time = 20;
 var timeOn = true;
-var correct = Math.floor(Math.random() * 4)
+var ranNum = Math.floor(Math.random() * 4);
+var letters = ["A. ","B. ","C. ","D. "];
+var options = [];
+var correctAnswer;
+var timeInterval = setInterval(countTime, 1000);
 
 function out(){
     $(".popup-overlay, .popup-content").removeClass("active");
@@ -8,24 +12,27 @@ function out(){
 
 function countTime(){
     if (time === 0){
-        timeOn = false;
         $('#time').addClass('hidden');
+        $('#question').addClass('hidden');
+        $('#answers').addClass('hidden');
         $(".popup-overlay, .popup-content").addClass("active");
+        $('.timeout').append(`<h5 id="pop">You didn't guess in time!
+        <br><br>The answer was ${correctAnswer}!</h5>`);
+        timeOn = false;
+        clearInterval(timeInterval);
     } else if (timeOn == true){
         time--;
         $('#time').text("Time Remaing: " + time);
     }
 }
-setInterval(countTime, 1000);
+setInterval(timeInterval);
 
     $('#start').on('click', function(){
         $('#start').addClass('hidden');
         $('#trivia').html(`<h3 id="time">Time Remaing: ${time}</h3>
             <h4 id="question"></h4>
-            <h5 id="a"></h5>
-            <h5 id="b"></h5>
-            <h5 id="c"></h5>
-            <h5 id="d"></h5>
+            <div id="answers">
+            </div>
             <!--Creates the popup body-->
             <div class="popup-overlay">
                 <!--Creates the popup content-->
@@ -40,19 +47,11 @@ setInterval(countTime, 1000);
             method: 'GET'
         }).then(function(response){
             $('#question').html(response.results[0].question);
-            var qArr = ["a","b","c","d"];
-            var ranQ = qArr[correct];
-            var letter = $(`#${ranQ}`);
-            letter.html(response.results[0].correct_answer);
+            correctAnswer = response.results[0].correct_answer;
+            options.push(response.results[0].incorrect_answers);
+            options[0].splice(ranNum, 0, correctAnswer);
+            for(var i=0; i<4; i++){
+                $('#answers').append(`<div id="choices"><span>${letters[i]}</span>${options[0][i]}</div>`);
+            }
         }); 
     });
-
-   
-
-    // var qurl = "https://opentdb.com/api.php?amount=10&type=multiple"
-    //     $.ajax({
-    //         url: qurl,
-    //         method: 'GET'
-    //     }).then(function(response){
-    //         $('question').html(response.results[1].question);
-    //     }); 
